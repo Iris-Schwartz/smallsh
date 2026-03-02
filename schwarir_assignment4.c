d49c8366869f60213b92b9ed1f1ae1d2e2680a1d
Iris Schwartz <schwarir@os1.engr.oregonstate.edu>
2026-03-01 20:29:25 -0800

    assigns sa_mask of signal handler to SA_RESTART so fgets is restarted if interrupted by SIGTSTP (CTRL-Z); removes unnecessary traversal of background_processes array during reaping of zombie child processes as well unnecessary printing of name of background process terminated by signal and related background_process struct; moves reaping of terminated zombie background child processes to BEFORE printing of command prompt; adds additional explanatory comments in code; adds extra fflush commands where needed; implements snake_case consistently

--- schwarir_assignment4.c
+++ schwarir_assignment4.c
@@ -1,44 +1,11 @@
 /*
-TO DO:
--- reminder to change all names to snake_case
--- add fflush where helpful to print stdout buffer contents to terminal
--- Ask TA about the following bugs:
-
-: date
-Fri Feb 27 06:49:35 AM PST 2026
-: ^Z
-Exiting foreground-only modeFri Feb 27 06:49:38 AM PST 2026
-
-
-: sleep 5 &
-background pid is 2604497
-: ^Z
-Entering foreground-only mode (& is now ignored)sleep: missing operand
-Try 'sleep --help' for more information.
-
-
--- add the following signal handling (✅ if finished)
-
------------------------------SIGINT-------------------------------SIGTSTP
-
-shell                        ignore  ✅                            *** ✅
-
-background_process           ignore    ✅ inherited from shell     ignore ✅
-
-foreground_process         foreground process                     ignore ✅
-                             terminates itself✅
-
-                            parent immediately ✅
-                             prints which
-                          signal killed foreground process
-
-*** The shell must display an informative message (see below) immediately if it's sitting at the prompt, or immediately after any currently running foreground process has terminated
-The shell then enters a state where subsequent commands can no longer be run in the background.
-In this state, the & operator must simply be ignored, i.e., all such commands are run as if they were foreground processes.
-
-If the user sends SIGTSTP again, then your shell will
-Display another informative message (see below) immediately after any currently running foreground process terminates
-The shell then returns back to the normal condition where the & operator is once again honored for subsequent commands, allowing them to be executed in the background.
+Author: Iris Schwartz
+OSU Email Address: schwarir@oregonstate.edu
+Course Number: CS 374
+Programming Assignment: 4
+Due Date: 03/01/26
+Description: This program creates a shell called smallsh that, based on input prompted by the command prompt :, can three built-in commands of cd, exit and status and can fork foreground and background child processes that execute the programs of files related to other commands (such as sleep, ls, ps, kill, etc.). This program also handles redirection of stdout and stdin based on < and > in the input, respectively. In addition, this program handles signals by either ignoring them, implenting their default actions or changing their disposition with custom signal handlers. Of note, the shell registers
+and installs a custom signal handler for toggling between regular mode and foreground-only mode, where background commands (indicated with &) are interpreted as foreground commands. Lastly, before the next command prompt : is printed, the parent shell checks for children that have terminated but are still in ps table and reaps (removes from ps table) these terminated children with waitpid() system call.
 */
 
 #define _POSIX_C_SOURCE 200809L // ChatGPT recommended adding this due to issues creating struct sigaction instance
@@ -73,15 +40,132 @@ struct background_process
 };
 
 int last_index_in_background_processes_array = 0;
-struct background_process background_processes[512];
+int background_processes[512];
 int has_run_foreground_command = 0;
-int lastExitStatus = 0;
-int toggleValue = 0;
+int last_exit_status = 0;
+int toggle_value = 0;
 
 void redirect(struct command_line *curr_command);
 int background_command(struct command_line *curr_command);
 int foreground_command(struct command_line *curr_command);
 void SIGTSTP_prevent_background_commmands_toggle(int signal);
+struct command_line *parse_input();
+
+int main()
+{
+  struct command_line *curr_command;
+
+  // in discussing reasoning for registering and installing the SIGINT shell signal handler right after the printing of the smallsh : command prompt (to ignore SIGINT in the shell) ChatGPT provided a strong argument for installing it at the start of main
+
+  // program code for signal handler installation was adapted from OSU's CS 374's "Exploration: Signal Handling API" lesson
+  // in module 7 (provided by Professor Tonsmann)
+
+  // registering the CTRL-C or SIGINT shell signal handler
+  struct sigaction ignore_SIGINT_signal = {0};
+  ignore_SIGINT_signal.sa_handler = SIG_IGN;
+  sigfillset(&ignore_SIGINT_signal.sa_mask);
+  ignore_SIGINT_signal.sa_flags = 0;
+
+  // installing the CTRL-C or SIGINT shell signal handler
+  sigaction(SIGINT, &ignore_SIGINT_signal, NULL);
+
+  // registering signal handler for CTRL-Z (i.e. SIGTSTP) toggle of foreground-only mode
+  struct sigaction SIGTSTP_handler = {0};
+  sigfillset(&SIGTSTP_handler.sa_mask);
+  SIGTSTP_handler.sa_handler = SIGTSTP_prevent_background_commmands_toggle;
+  // suggested by TA to solve issue of fgets not being restarted from where it left off when it gets interrupted by CTRL-Z and instead of being restarted, returning NULL and executing a previous command entered in smallsh shell (vs current command)
+  SIGTSTP_handler.sa_flags = SA_RESTART;
+
+  // install CTRL-Z foreground-only mode toggle signal handler
+  sigaction(SIGTSTP, &SIGTSTP_handler, NULL);
+
+  while (true)
+  {
+    // the shell constantly checks if any background child process just finished before parent shell (i.e. a zombie) and reaps it
+    // (removes it from ps table) if so before the next command prompt is printed
+    // assistance was provided by TA to generate the proper while loop header
+
+    // among the various ideas ChatGPT gave in solving the issue of exit value of background process
+    // not printing right away, of which multiple I tried implenting, was looking into whether
+    // the exit value of a background process that just finished wasn't finished until after the
+    // next command prompt : of smallsh shell was printed
+
+    int child_status;
+    int child_pid;
+
+    while ((child_pid = waitpid(-1, &child_status, WNOHANG)) > 0)
+    {
+      if (WIFEXITED(child_status))
+      {
+        printf("background pid %d is done: exit value %d\n", child_pid, WEXITSTATUS(child_status));
+        fflush(stdout);
+      }
+      else
+      {
+        printf("background pid %d is done: terminated by signal %d\n", child_pid, WTERMSIG(child_status));
+        fflush(stdout);
+      }
+    }
+    curr_command = parse_input();
+    if (curr_command->argc == 0 || curr_command->argv[0][0] == '#')
+    {
+      continue;
+    }
+    else if (strcmp(curr_command->argv[0], "status") == 0)
+    {
+      if (has_run_foreground_command == 0)
+      {
+        printf("exit value 0\n");
+        fflush(stdout);
+      }
+      else
+      {
+        printf("exit value %d\n", last_exit_status);
+        fflush(stdout);
+      }
+    }
+    else if (strcmp(curr_command->argv[0], "exit") == 0)
+    {
+      // kills all background processes that shell has previously started before the shell terminates itself (note: they will eventually also be reaped, or removed from ps table, by init, since parent process does not reap them here)
+      for (int i = 0; i <= last_index_in_background_processes_array; i++)
+      {
+        kill(background_processes[i], SIGTERM);
+      }
+      exit(0);
+    }
+    else if (strcmp(curr_command->argv[0], "cd") == 0)
+    {
+      if (curr_command->argv[1] != NULL)
+      {
+        if (chdir(curr_command->argv[1]) == -1)
+        {
+          perror("Issue changing to a certain directory");
+        }
+      }
+      else
+      {
+        if (chdir(getenv("HOME")) == -1)
+        {
+          perror("Issue changing to home directory");
+        };
+      }
+    }
+    else
+    {
+      // short circuit evaluation so that foreground command executed as
+      // soon as toggle_value == 0 evaluates to false regardless of value of curr_command->is_bg
+      if (toggle_value == 0 && curr_command->is_bg)
+      {
+        background_command(curr_command);
+      }
+      else
+      {
+        last_exit_status = foreground_command(curr_command);
+      }
+    }
+  }
+  return EXIT_SUCCESS; // exits from shell (parent process)
+}
 
 // this function was provided as started code by Professor Tonsmann from OSU's CS 374 class for
 // the purposes of this assignment
@@ -120,117 +204,6 @@ struct command_line *parse_input()
   return curr_command;
 }
 
-int main()
-{
-  struct command_line *curr_command;
-
-  // in discussing my reasoning for installing the signal handler right after the printing of the smallsh : command prompt (to ignore SIGINT in the shell) ChatGPT provided a strong argument for installing it at the start of main
-
-  // program code for signal handler installation was adapted from OSU's CS 374's "Exploration: Signal Handling API" lesson
-  // in module 7 (provided by Professor Tonsmann)
-  struct sigaction ignore_SIGINT_signal = {0};
-
-  ignore_SIGINT_signal.sa_handler = SIG_IGN;
-  sigfillset(&ignore_SIGINT_signal.sa_mask);
-  ignore_SIGINT_signal.sa_flags = 0;
-
-  sigaction(SIGINT, &ignore_SIGINT_signal, NULL);
-
-  while (true)
-  {
-    curr_command = parse_input();
-    if (curr_command->argc == 0 || curr_command->argv[0][0] == '#')
-    {
-      continue;
-    }
-    else if (strcmp(curr_command->argv[0], "status") == 0)
-    {
-      if (has_run_foreground_command == 0)
-      {
-        printf("exit value 0\n");
-      }
-      else
-      {
-        printf("exit value %d\n", lastExitStatus);
-      }
-    }
-    else if (strcmp(curr_command->argv[0], "exit") == 0)
-    {
-      // kills all background processes that shell has previously started before it terminates itself (note: they will eventually also be reaped, or removed from ps table, by init, since parent process does not reap them here)
-      for (int i = 0; i <= last_index_in_background_processes_array; i++)
-      {
-        kill(background_processes[i].background_process_pid, SIGTERM);
-      }
-      exit(0);
-    }
-    else if (strcmp(curr_command->argv[0], "cd") == 0)
-    {
-      if (curr_command->argv[1] != NULL)
-      {
-        if (chdir(curr_command->argv[1]) == -1)
-        {
-          perror("Issue changing to a certain directory");
-        }
-      }
-      else
-      {
-        if (chdir(getenv("HOME")) == -1)
-        {
-          perror("Issue changing to home directory");
-        };
-      }
-    }
-    else
-    {
-      // short circuit evaluation so that foreground command executed as
-      // soon as toggleValue == 0 evaluates to false regardless of value of curr_command->is_bg
-      struct sigaction SIGTSTP_handler = {0};
-
-      sigfillset(&SIGTSTP_handler.sa_mask);
-      SIGTSTP_handler.sa_handler = SIGTSTP_prevent_background_commmands_toggle;
-      SIGTSTP_handler.sa_flags = 0;
-
-      // install signal handler in shell
-      sigaction(SIGTSTP, &SIGTSTP_handler, NULL);
-
-      if (toggleValue == 0 && curr_command->is_bg)
-      {
-
-        background_command(curr_command);
-      }
-      else
-      {
-        lastExitStatus = foreground_command(curr_command);
-      }
-    }
-
-    int childStatus;
-    int childPid;
-    // the shell constantly checks if any background child process just finished and reaps it
-    // (removes it from ps table) if so before the next command prompt is printed
-    // assistance was provided by TA to generate the proper while loop header
-    while ((childPid = waitpid(-1, &childStatus, WNOHANG)) > 0)
-    {
-      if (WIFEXITED(childStatus))
-      {
-        for (int i = 0; i < 512; i++)
-        {
-          if (background_processes[i].background_process_pid == childPid)
-          {
-            printf("background pid %d is done: exit value 0\n", background_processes[i].background_process_pid);
-            printf("the background %s finally finished\n", background_processes[i].background_process_name);
-          }
-        }
-      }
-      else
-      {
-        printf("%d", WTERMSIG(childStatus));
-      }
-    }
-  }
-  return EXIT_SUCCESS; // exits from shell (parent process)
-}
-
 void redirect(struct command_line *curr_command)
 {
   if (curr_command->output_file != NULL)
@@ -239,11 +212,13 @@ void redirect(struct command_line *curr_command)
     if (fd1 == -1)
     {
       printf("cannot open %s for output", curr_command->output_file);
+      fflush(stdout);
       exit(1);
     }
     if (dup2(fd1, 1) == -1)
     {
       printf("Error redirecting stdout to output file.");
+      fflush(stdout);
       exit(1);
     };
     if (curr_command->is_bg)
@@ -252,6 +227,7 @@ void redirect(struct command_line *curr_command)
       if (dup2(fd2, 0) == -1)
       {
         printf("Error redirecting stdin to input file.");
+        fflush(stdout);
         exit(1);
       };
     }
@@ -263,11 +239,13 @@ void redirect(struct command_line *curr_command)
     if (fd2 == -1)
     {
       printf("cannot open %s for input", curr_command->input_file);
+      fflush(stdout);
       exit(1);
     }
     if (dup2(fd2, 0) == -1)
     {
       printf("Error redirecting stdin to input file.");
+      fflush(stdout);
       exit(1);
     };
     if (curr_command->is_bg)
@@ -276,6 +254,7 @@ void redirect(struct command_line *curr_command)
       if (dup2(fd2, 1) == -1)
       {
         printf("Error redirecting stdout to output file.");
+        fflush(stdout);
         exit(1);
       };
     }
@@ -287,91 +266,99 @@ void SIGTSTP_prevent_background_commmands_toggle(int signal)
 {
   // if currently not in foreground only mode, then in response to SIGTSTP, switch to foreground
   // only mode
-  if (toggleValue == 0)
+  if (toggle_value == 0)
   {
-    toggleValue = 1;
+    toggle_value = 1;
     // reentrant function used (not printf())
-    write(1, "\nEntering foreground-only mode (& is now ignored)\n", 49);
+    write(1, "\nEntering foreground-only mode (& is now ignored)", 51);
   }
   // if currently not in foreground only mode, then in response to SIGTSTP, switch to foreground
   // only mode
   else
   {
-    toggleValue = 0;
-    write(1, "\nExiting foreground-only mode\n", 29);
+    toggle_value = 0;
+    write(1, "\nExiting foreground-only mode", 31);
   }
 }
 
 int background_command(struct command_line *curr_command)
 {
+  // registering signal handler to ignore CTRL-Z to background command
   struct sigaction ignore_SIGTSTP_signal = {0};
   ignore_SIGTSTP_signal.sa_handler = SIG_IGN;
   sigfillset(&ignore_SIGTSTP_signal.sa_mask);
   ignore_SIGTSTP_signal.sa_flags = 0;
 
-  pid_t pidOfChild = fork();
+  pid_t child_pid = fork();
 
-  switch (pidOfChild)
+  switch (child_pid)
   {
   case -1:
     perror("issue with fork()");
     exit(1);
   case 0:
+    // installing signal handler to ignore CTRL-Z to background command
     sigaction(SIGTSTP, &ignore_SIGTSTP_signal, NULL);
     redirect(curr_command);
     if (execvp(curr_command->argv[0], curr_command->argv) == -1)
     {
       printf("%s: no such file or directory\n", curr_command->argv[0]);
+      fflush(stdout);
       exit(1);
     }
     break;
   default:
-    background_processes[last_index_in_background_processes_array].background_process_pid = pidOfChild;
-    background_processes[last_index_in_background_processes_array].background_process_name = curr_command->argv[0];
+    background_processes[last_index_in_background_processes_array] = child_pid;
     last_index_in_background_processes_array++;
-    printf("background pid is %d\n", pidOfChild);
+    printf("background pid is %d\n", child_pid);
+    fflush(stdout);
   }
 }
 
 int foreground_command(struct command_line *curr_command)
 {
-  int childStatus;
+  int child_status;
   has_run_foreground_command = 1; // needs to be updated in the parent b/c child update won't get remembered by parent
 
+  // registering signal handler to reverse the ignoring of CTRL-C from shell parent process
   struct sigaction reverse_ignore_from_shell = {0};
   reverse_ignore_from_shell.sa_handler = SIG_DFL;
 
+  // registering signal handler to ignore CTRL-Z to foreground command
   struct sigaction ignore_SIGTSTP_signal = {0};
   ignore_SIGTSTP_signal.sa_handler = SIG_IGN;
   sigfillset(&ignore_SIGTSTP_signal.sa_mask);
   ignore_SIGTSTP_signal.sa_flags = 0;
 
-  pid_t pidOfChild = fork();
+  pid_t child_pid = fork();
 
-  switch (pidOfChild)
+  switch (child_pid)
   {
   case -1:
     perror("issue with fork()");
     exit(1);
   case 0:
+    // installing signal handler to reverse the ignoring of CTRL-C from shell parent process
     sigaction(SIGINT, &reverse_ignore_from_shell, NULL);
+    // installing signal handler to ignore CTRL-Z to foreground command
     sigaction(SIGTSTP, &ignore_SIGTSTP_signal, NULL);
     redirect(curr_command);
     if (execvp(curr_command->argv[0], curr_command->argv) == -1)
     {
       printf("%s: no such file or directory\n", curr_command->argv[0]);
+      fflush(stdout);
       exit(1);
     }
     break;
   default:
-    pidOfChild = waitpid(pidOfChild, &childStatus, 0);
-    if (WIFEXITED(childStatus)) // if child terminated normally
+    child_pid = waitpid(child_pid, &child_status, 0); // shell is blocked; waits for foreground child process to finish before returning command line access;
+    if (WIFEXITED(child_status))                      // if child terminated normally
     {
-      return WEXITSTATUS(childStatus); // exit value the child passed to exit()
+      return WEXITSTATUS(child_status); // exit value the child passed to exit()
     }
     else // if child terminated abnormally
     {
-      int sigNum = WTERMSIG(childStatus);
+      int sigNum = WTERMSIG(child_status);
       printf("terminated by signal %d\n", sigNum);
       fflush(stdout);
       return sigNum; // signal number that caused the child to terminate
